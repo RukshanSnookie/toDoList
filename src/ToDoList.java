a7a35ba5c0f7c87d2be19152a694c03edcac1aa4
Rukshan Dissanayake <rukshanmali@gmail.com>
2023-12-04 00:11:52 +0530

    Exception handling using try catch

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -19,32 +19,40 @@ public class ToDoList {
 			System.out.println("\n4. Delete Task");
 			System.out.println("\n5. Save and Exit");
 			
-			int menuInput = scan.nextInt(); // taking user input from the menu
-			scan.nextLine(); // consume the new line character
-			
-			switch(menuInput) {
-			case 1: // adding Tasks
-				addTasks(); // calling add tasks method to add new task
-				break;
-			case 2:
-				viewTasks(); // calling the view tasks method
-				break;
-			case 3:
-				System.out.println("Tasks marking as completed : ");
-				markTasks(); // calling the mark  Tasks method
-				break;
-			case 4:
-				System.out.println("Deleting your tasks, are you sure? : ");
-				delTasks();  // calling the delete tasks method
-				break;
-			case 5:
-				System.out.println("Save and Exit!");
-				saveExit();  // calling the save and exit method
-				break;
-			default:
-				System.out.println("Something went wrong. Exiting the program");
-				isMenuOpen = false;  // exit the loop if something else happens
+			try {
+				int menuInput = scan.nextInt(); // taking user input from the menu
+				scan.nextLine(); // consume the new line character
+				
+				switch(menuInput) {
+				case 1: // adding Tasks
+					addTasks(); // calling add tasks method to add new task
+					break;
+				case 2:
+					viewTasks(); // calling the view tasks method
+					break;
+				case 3:
+					System.out.println("Tasks marking as completed : ");
+					markTasks(); // calling the mark  Tasks method
+					break;
+				case 4:
+					System.out.println("Deleting your tasks, are you sure? : ");
+					delTasks();  // calling the delete tasks method
+					break;
+				case 5:
+					System.out.println("Save and Exit!");
+					saveExit();  // calling the save and exit method
+					break;
+				default:
+					System.out.println("Something went wrong. Exiting the program");
+					isMenuOpen = false;  // exit the loop if something else happens
+				}
 			}
+			catch(Exception e) {
+				System.out.println("Invalid input : please enter a valid input");
+				scan.nextLine();
+				continue; // to restart the loop
+			}
+
 			
 		} // end of the while  loop
 		
@@ -56,27 +64,33 @@ public class ToDoList {
 		
 		System.out.println("Add your tasks here : ");
 		
-		String task = scan.next();
-		TaskWithStat.status stat = TaskWithStat.status.OPEN; // using the enum value OPEN
-		
-		TaskWithStat taskStat = new TaskWithStat(task,stat); // creating an object with both task and status
-		
-		taskList.add(taskStat); // add task with status to the array list
-		
-		System.out.println("\nYour task added to the list successfully!!");
-		System.out.println("\nGo back to Menu or Exit: (y/n)");
-		String response = scan.next().toLowerCase(); // storing the next user input as lower case 
-		
-		if(response.equals("n")) {
-			System.out.println("See you again!!");
-			isMenuOpen = false; // stopping the current loop
+		try {
+			String task = scan.nextLine();
+			TaskWithStat.status stat = TaskWithStat.status.OPEN; // using the enum value OPEN
+			
+			TaskWithStat taskStat = new TaskWithStat(task,stat); // creating an object with both task and status
+			
+			taskList.add(taskStat); // add task with status to the array list
+			
+			System.out.println("\nYour task added to the list successfully!!");
+			System.out.println("\nGo back to Menu or Exit: (y/n)");
+			String response = scan.next().toLowerCase(); // storing the next user input as lower case 
+			
+			if(response.equals("n")) {
+				System.out.println("See you again!!");
+				isMenuOpen = false; // stopping the current loop
+			}
+			else if(response.equals("y")) {
+				return; // return to menu
+			}
+			else {
+				System.out.println("you kicked out! :( ");
+				isMenuOpen= false; // stopping the current loop
+			}
+			
 		}
-		else if(response.equals("y")) {
-			return; // return to menu
-		}
-		else {
-			System.out.println("you kicked out! :( ");
-			isMenuOpen= false; // stopping the current loop
+		catch(Exception e) {
+			System.out.println(e);
 		}
 		
 	}
