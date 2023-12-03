300e08e64c4251d34715a8cca9f077589c5edc1e
Rukshan Dissanayake <rukshanmali@gmail.com>
2023-12-04 01:40:47 +0530

    updated functionalities

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -19,7 +19,7 @@ public class ToDoList {
 			System.out.println("\n4. Delete Task");
 			System.out.println("\n5. Save and Exit");
 			
-			try {
+			try { // try with resources . scanner to close automatically
 				int menuInput = scan.nextInt(); // taking user input from the menu
 				scan.nextLine(); // consume the new line character
 				
@@ -31,7 +31,6 @@ public class ToDoList {
 					viewTasks(); // calling the view tasks method
 					break;
 				case 3:
-					System.out.println("Tasks marking as completed : ");
 					markTasks(); // calling the mark  Tasks method
 					break;
 				case 4:
@@ -73,7 +72,64 @@ public class ToDoList {
 			taskList.add(taskStat); // add task with status to the array list
 			
 			System.out.println("\nYour task added to the list successfully!!");
-			System.out.println("\nGo back to Menu or Exit: (y/n)");
+			backToMenu();
+			
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+	public static void viewTasks() {
+		System.out.println("\nYour Tasks are showing below : ");
+		taskList.forEach(items -> System.out.println(items)); // for each loop to iterate arraylist and print items
+		backToMenu();
+	}
+	
+	public static void markTasks() {
+		
+		try {
+			System.out.println("\nEnter your task name : ");
+			
+			boolean taskFound = false;
+			String taskName = scan.next(); //taking user input, task name
+			
+			
+			for(TaskWithStat items : taskList) {  //iterating through the tasks list
+				if(items.task.equalsIgnoreCase(taskName)) {  // checking the case sensitive comparison
+					items.stat = TaskWithStat.status.COMPLETED;  // update the status if user input matches the item
+					taskFound = true;
+					System.out.println("\nTask Updated" + items);
+					break;
+				}
+			}
+			
+			
+			if(!taskFound) {
+				System.out.println(taskName + " Not Found");
+			}
+			backToMenu(); // calling back t menu function 
+			
+		}
+		catch(Exception e) {
+			System.out.println("Invalid Input : Please try again!!");
+			
+		}
+	}
+	
+	public static void delTasks() {
+		System.out.println("in del tasks");
+	}
+	
+	public static void saveExit() {
+		System.out.println("Bye Bye!");
+		isMenuOpen = false;
+	}
+	
+	public static void backToMenu() {
+		System.out.println("\nGo back to Menu or Exit: (y/n)");
+
 			String response = scan.next().toLowerCase(); // storing the next user input as lower case 
 			
 			if(response.equals("n")) {
@@ -86,48 +142,10 @@ public class ToDoList {
 			else {
 				System.out.println("you kicked out! :( ");
 				isMenuOpen= false; // stopping the current loop
-			}
-			
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
+			}		
 		
 	}
-	
-	public static void viewTasks() {
-		System.out.println("\nYour Tasks are showing below : ");
-		taskList.forEach(items -> System.out.println(items)); // for each loop to iterate arraylist and print items
-		
-		System.out.println("\nGo back to Menu or Exit: (y/n)");
-		String response = scan.next().toLowerCase(); // storing the next user input as lower case 
-		
-		if(response.equals("n")) {
-			System.out.println("See you again!!");
-			isMenuOpen = false; // stopping the current loop
-		}
-		else if(response.equals("y")) {
-			return; // return to menu
-		}
-		else {
-			System.out.println("you kicked out! :( ");
-			isMenuOpen= false; // stopping the current loop
-		}
-	}
-	
-	public static void markTasks() {
-		System.out.println("in mark tasks");
-	}
-	
-	public static void delTasks() {
-		System.out.println("in del tasks");
-	}
-	
-	public static void saveExit() {
-		System.out.println("Bye Bye!");
-		isMenuOpen = false;
-	}
-	
+
 }
 
 
